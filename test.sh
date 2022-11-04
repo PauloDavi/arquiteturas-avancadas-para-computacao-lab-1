83ae99c066c8adb19fa1b8bd3c72b17b2241d1ba
PauloDavi <araujo.paulo.davi@gmail.com>
2022-11-03 21:59:55 -0300

    improve prints

--- test.sh
+++ test.sh
@@ -1,18 +1,23 @@
 DXS="100 1000 10000 100000 1000000 10000000 100000000"
+GREEN='\033[1;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 clear
 
-echo "Single Thread"
+echo "${GREEN}Single Thread${NC}"
 
 for DX in ${DXS}; do
+  echo "${BLUE}DX = ${DX}${NC}"
   make run_single ARGS=${DX} | grep -E "Time|Valor da integral de pi:" | tail -1;
   echo;
 done
 
-echo "Multi Thread"
+echo "\n${GREEN}Multi Thread${NC}"
 
 for THREAD_NUMBER in $(seq 2 8); do
   for DX in ${DXS}; do
+    echo "${BLUE}Threads = ${THREAD_NUMBER}\nDX = ${DX}${NC}"
     make run_thread ARGS="${THREAD_NUMBER} ${DX}" | grep -E "Time|Valor da integral de pi:" | tail -1;
     echo;
   done
